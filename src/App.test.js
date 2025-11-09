94dba63d62c5a6e98ad1e5ad9ef204a73bb2d327
Sam-Safari <samuelemanman@gmail.com>
2025-11-09 19:35:22 +0300

    fix(tests): update import paths in App.test.js after moving test files

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import './AllPlants.test';
-import './CreatePlant.test';
-import './InStock.test';
-import './SearchPlants.test';
+import './test_suites/AllPlants.test';
+import './test_suites/CreatePlant.test';
+import './test_suites/InStock.test';
+import './test_suites/SearchPlants.test';
